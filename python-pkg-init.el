5c7717f36c9698dbb8afbb4702d75aa882c4d501
Danila <ddbihbka@gmail.com>
2014-09-04 13:04:23 +0400

    Check python and pip in system

--- python-pkg-init.el
+++ python-pkg-init.el
@@ -1,4 +1,10 @@
 
+(when (not (ignore-errors (el-get-executable-find "python")))
+  (error "Can't find python."))
+(when (not (ignore-errors (el-get-executable-find "pip")))
+  (error "Can't find pip."))
+
+
 (add-to-list 'my:el-get-packages 'python)
 
 (if (ignore-errors (el-get-executable-find "virtualenv"))
